0545f19d2f6cbbab94cd94e8e2df7e91457d248e
NeFaWaltPint <nefawaltpint@gmail.com>
2024-10-03 21:01:00 -0500

    fix: ajuste consumo correos

--- controllers/gestion_correos.go
+++ controllers/gestion_correos.go
@@ -40,7 +40,7 @@ func (c *GestionCorreosController) SugerenciaCorreoInstitucional() {
 	}
 
 	// Opcion
-	opcion, err := c.GetInt64("Opcion")
+	opcion, err := c.GetInt64("opcion")
 	if err != nil {
 		resultado := requestresponse.APIResponseDTO(false, 403, "Error obteniendo Opcion")
 		c.Ctx.Output.SetStatus(resultado.Status)
